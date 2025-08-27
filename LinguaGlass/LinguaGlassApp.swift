f828da74a488b0ab29b88594b7eab63de9a16494
Steven Tran <stevenvinhtran007@gmail.com>
2025-08-27 11:11:07 -0400

    Initial Commit

---
+++ LinguaGlass/LinguaGlassApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinguaGlassApp.swift
+//  LinguaGlass
+//
+//  Created by Steven Tran on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct LinguaGlassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
